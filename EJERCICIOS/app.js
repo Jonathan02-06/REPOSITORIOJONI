05e6622818c15639544586428d5f1109154015bc
jonathan02-06 <jonathan_str@hotmail.com>
2024-09-25 22:16:04 +0200

    25/09/24

---
+++ EJERCICIOS/app.js
@@ -0,0 +1,52 @@
+
+//*iteracion#1
+
+//*1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+let myFavoriteHero = "hulk";
+
+console.log(myFavoriteHero);
+
+//*1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+let x = 50;
+
+console.log(x);
+
+//*1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+
+let h = 5;
+let y = 10;
+
+console.log(h, y);
+
+//*1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+let z = h + y;
+
+console.log(h + y);
+
+//*Iteración#2
+
+//*1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+
+console.log(character);
+
+//*1.2 Declara 3 variables con los nombres y valores siguientes 
+//*firstName = 'Jon'; 
+//*lastName = 'Snow'; 
+//*age = 24; 
+//*Muestralos por consola de esta forma: 
+//*'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+let textoEntero = 'soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.';
+
+console.log(textoEntero);
+
+//*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+//*ambos juguetes
---
+++ TEORIA/app.js
@@ -0,0 +1,35 @@
+let saludo = "hola";
+
+let saludoArray = Array.from(saludo);
+console.log(saludoArray);
+
+//ejemplo array
+let x = [1, 2, 3, 4];
+let copia = Array.from(x);
+console.log(x);
+console.log(copia);
+console.log(x == copia);
+
+
+/// mAP
+// CREA NUEVA ARRAY CON LOS RESULTADOS DE APLICAR UNA FUNCION A LOS ELEMENTOS DE OTRO ARRAY
+// NO MODIFICA
+
+//condicionales
+/// if  if(condicion)  {instruccion}
+//if else (varias condicones)
+
+if (10 > 1) {
+    console.log('todo bien');
+}
+ if (1 > 10){
+    console.log('todo bien');
+ }
+let numero = 200;
+
+ if ( numero % 2 == 0 ){
+    console.log('par');
+ } else {
+    console.log('impar');
+ }
+   //switch
