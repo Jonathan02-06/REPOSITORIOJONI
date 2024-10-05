8109c0af02e70d5967021ca1c046c900ddb20295
jonathan02-06 <jonathan_str@hotmail.com>
2024-10-05 17:13:21 +0200

    ejercicioshastaitineration6

--- EJERCICIOS/app.js
+++ EJERCICIOS/app.js
@@ -97,4 +97,105 @@ let i = c
 i*= m
 console.log(i);
 
+//*Iteración#4
+
+//*.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+//*const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"]
+console.log(avengers[0])
+
+//*1.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+avengers[0] = "ironman"
+console.log(avengers);
+
+//*1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+
+console.log(avengers.length);
+
+//*1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//*Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"]
+rickAndMortyCharacters.push('Morty', 'Summer')
+console.log(rickAndMortyCharacters);
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1])
+
+//*1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+//*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters.push('Lapiz Lopez')
+console.log(rickAndMortyCharacters);
+rickAndMortyCharacters.pop()
+console.log(rickAndMortyCharacters);
+console.log("Primer personaje:", rickAndMortyCharacters[0])
+console.log("Último personaje:", rickAndMortyCharacters[rickAndMortyCharacters.length - 1])
+
+//*Elimina el segundo elemento del array y muestra el array por consola.
+//*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters.push('Lapiz Lopez')
+console.log(rickAndMortyCharacters)
+rickAndMortyCharacters.splice(1, 1)
+console.log(rickAndMortyCharacters)
+
+//*Iteración #5
+
+//*En base al código siguiente, muestra los mensajes correctos por consola.
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+//*if (/* COMPLETAR */) 
+
+if(number2 / number2 ===2) {
+    console.log("number2 dividido entre number1 es igual a 2")
+}
+if (number1 !== number2) { 
+    console.log("number1 es estrictamente distinto a number2")
+} 
+if (number3 !== number1) { 
+    console.log("number3 es distinto number1") 
+} 
+if (number3 * 5 === number1) { 
+    console.log("number3 por 5 es igual a number1") 
+} 
+if (number3 * 5 === number1 && number1 * 2 === number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+if (number2 / 2 === number1 || number1 / 5 === number3) {
+    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
+//*Iteración #6
+
+//*1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+for(let i=0; i<= 9; i++) {
+    console.log(i);
+}
+
+//*1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+//*cuando el resto del numero dividido entre 2 sea 0.
+
+for(let i=0; i<= 9; i++) {
+    if (i % 2 === 0) {
+        console.log(i); 
+    }
+}
+
+//*1.3 Crea un bucle para conseguir dormir contando ovejas.
+//*Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+//*Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle
+//*y cambia el mensaje en la décima vuelta a 'Dormido!'.
+
+for(let i= 0; i<= 10; i++) {
+    if (i === 10) {
+        console.log('DORMIDO');
+        } else {
+        console.log('INTENTANDO DORMIR 🐑');
+        }
+    }
 
--- TEORIA/app.js
+++ TEORIA/app.js
@@ -33,3 +33,98 @@ let numero = 200;
     console.log('impar');
  }
    //switch
+
+   const listaAlumnos = ['richard', 'antonio', 'luis'];
+
+let nombresAlumnos = listaAlumnos.forEach(
+    function (elementodelarray) {
+        console.log(elementodelarray);
+    }
+)
+/////
+const numeros = [100, 200, 300];
+
+for (let cadaelemento of numeros) {
+   cadaelemento *= 8;
+   console.log(cadaelemento);
+}
+
+const cosas = [
+   'taza',
+   'lavadora',
+   'mesa'
+];
+
+for (const cosa in cosas) {
+   console.log(`algo de la cocina es ${cosas[cosa]}`);
+};
+
+let personaje = {
+   nombre : 'zinedine',
+   apellido : 'zidane',
+   ciudad : 'marsella',
+   instrumento : 'balon'
+}
+
+for (const claves in personaje) {
+   console.log(`estas son las claves de personaje: ${personaje[claves]}`)
+};
+
+
+///metodos
+
+let listanumeros = [1, 2, 3, 4, 5];
+
+let listanumeroscuadrado = listanumeros.map(
+   function (elemento) {
+      return elemento * elemento
+   }
+)
+console.log(listanumeroscuadrado);
+
+///calcular cuadrado solo de nuemros pares y si no scaamos el original
+
+let listacuadradospares = listanumeros.map(
+   function (elemento) {
+      if (elemento % 2 == 0) {
+         return elemento * elemento
+      } else {
+         return elemento
+      }
+   }   
+)
+
+// igual que el anterior pero en ternario
+//condicion true o false
+
+let ternariocuadradospares = listanumeros.map(
+   function (elemento) {
+      return elemento % 2 == 0 
+      ? elemento * elemento
+      : elemento
+   }   
+)
+console.log(ternariocuadradospares);
+
+
+//mismo ejercios forma simplificada
+
+let ternarioarrow = listanumeros.map(elemento => elemento % 2 == 0 ? elemento * elemento : elemento)
+console.log(ternarioarrow);
+
+///FILTER 
+//DEVUELVE LOS ELEMNTOS DE UN
+// ARRAY QUE CUMPLEN UNA CONDICON
+
+//*SCAAMOS LOS PARES DE UNA LISTA
+
+const cifras = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+ let pares = cifras.filter(e => e % 2 == 0)
+ console.log (pares);
+
+ ///ejemplo JSON
+
+ let dataJSON = `{ "nombre" : "noelia" : "hobby" : "nadar" , "numeros" : [ 0, 13 ]}`
+console.log(dataJSON);
+
+
